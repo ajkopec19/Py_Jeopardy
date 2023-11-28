7bd45a4a19c60f8857ff179c02ede7f04530a3f8
AJ <ajkopec19@gmail.com>
2023-11-28 18:15:40 -0500

    added the categories

---
+++ game.py
@@ -0,0 +1,21 @@
+#need question class
+#need game function
+#need to make the questions
+#separate function for answering the questions(?)
+
+def categories():
+    category = {("Eighties Lyrics", "The answers is the name of the song the lyrics are from."), 
+                ("Some Nostaglia For Ya", "All answers will pertain to things during the 2000s and early 2010s."),
+                ("Be a Good Sport", "All answers will pertain to sports-related things.")
+                ("Astronomy", "All answers will pertain to astronomy.")
+                ('''Sorry, I Have a "Nut" Allergy''', '''All answers will contain the word "nut".''')
+                ("The Apple of my Eye", "All answers pertain to apples.")
+                ("Discrete Math", "No, not Discreet. All answers have to do with discrete math. Sorry Com Sci kids.")
+                ("Finish the Phrase", "All answers will the word missing from the phrase.")
+                ("Geology in Geography", "All answers will be the location of the mentioned geological formation or event.")
+                ("HOMES", "All answers pertain to the Great Lakes from Michigan.")
+                ("Shocking!", "All answers pertain to weather, specifically thunderstorms.")
+                ("Dog", "All answers will pertain to Dogs")
+                }
+    return
+
