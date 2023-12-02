92628d250c5865958b34f96a8e95e2269190942c
AJ <ajkopec19@gmail.com>
2023-12-02 16:09:23 -0500

    added classes and a couple functions

--- game.py
+++ game.py
@@ -2,11 +2,30 @@
 #need game function
 #need to make the questions
 #separate function for answering the questions(?)
+import random
 
+class QnA:
+    def __init__(self, question, answer):
+        self.question = question
+        self.answer = answer
+    def getQuestion(self):
+        return self.question
+    def getAnswer(self):
+        return self.answer
+    
+class Category:
+    def __init__(self, category, desc):
+        self.category = category
+        self.desc = desc
+    def getCategory(self):
+        return self.category
+    def getDesc(self):
+        return self.desc
+    
 def categories():
     category = {("Eighties Lyrics", "The answers is the name of the song the lyrics are from."), 
                 ("Some Nostaglia For Ya", "All answers will pertain to things during the 2000s and early 2010s."),
-                ("Be a Good Sport", "All answers will pertain to sports-related things."),
+                ("Be a Good Sport", "All answers will pertain to sports."),
                 ("Astronomy", "All answers will pertain to astronomy."),
                 ('''Sorry, I Have a "Nut" Allergy''', '''All answers will contain the word "nut".'''),
                 ("The Apple of my Eye", "All answers pertain to apples."),
@@ -36,7 +55,37 @@ def sport():
     }
     return sportQuestions
 
+def nostalgia():
+    nostalgiaQuestions = {
+        ("This soft, colorful toy may have sounded edible, but it is, in fact, not. However, you could mold it into tasty foods.", 
+         "play-doh"),
+        ("Spinning-top toys ha,ve always been a classic, but this toy turned them into things you could use to battle other people in a plastic arena.",
+         "beyblade")
+        ("The phone is ringing! This kids show that ended in 2016 followed a trio of animals rescuing other animals.",
+          "wonder pets"),
+        ()
+
+        
+    }
+# insert more categories below this after further testing
+
+def dailyDouble():
+    print("yeye")
+
+def getGuess():
+    valid = True
+    while valid:
+        guess = input("Enter your answer: ")
+        try:
+            assert "what is" in guess.lower() or "who is" in guess.lower()
+            valid = False
+        except:
+            print('''Answer must contain "Who is" or "What is"''')
+    return guess
+
+
 def game():
-    print("bruh")
+    guess = getGuess()
+    print(guess)
 
 game()
